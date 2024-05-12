009d22bfebee1bae41b19de74edcd041902ca2ee
leephan2k1 <pttl2k1@gmail.com>
2024-05-12 17:20:21 +0700

    add fuzzy search utility

--- src/utils/tools/functions.ts
+++ src/utils/tools/functions.ts
@@ -1,5 +1,17 @@
+import Fuse, { IFuseOptions } from 'fuse.js';
+
 export function getMethodName() {
   const err = new Error();
   // @ts-ignore: we want the 2nd method in the call stack
   return /at \w+\.(\w+)/.exec(err.stack.split('\n')[2])[1];
+}
+
+export function fuzzySearch(
+  keyword: string,
+  list: Array<any>,
+  options?: IFuseOptions<any>,
+) {
+  const fuse = new Fuse(list, options);
+
+  return fuse.search(keyword);
 }
