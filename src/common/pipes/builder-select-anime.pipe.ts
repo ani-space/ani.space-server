8d483294f885338648552dcc651886295f91dde7
leephan2k1 <pttl2k1@gmail.com>
2024-06-23 13:19:22 +0700

    dynamic skips error fields

--- src/common/pipes/builder-select-anime.pipe.ts
+++ src/common/pipes/builder-select-anime.pipe.ts
@@ -4,12 +4,23 @@ import {
   MapResultSelect,
   reverseBooleanValueInObj,
 } from '../../utils/tools/object';
+import { NotFoundAnimeError } from '../../graphql/types/dtos/anime-response/not-found-anime.error';
+import { QueryAnimeArg } from '../../graphql/types/args/query-anime.arg';
 
 export class BuilderSelectAnimePipe implements PipeTransform {
   transform(value: any): MapResultSelect {
+    /*
+     * We don't want get fields from Error query and pass to Select TypeORM
+     */
+    const fieldsToSkip = Object.getOwnPropertyNames(
+      new NotFoundAnimeError({
+        requestObject: new QueryAnimeArg(),
+      }),
+    );
+
     const mappedSelect = reverseBooleanValueInObj(
       fieldsMap(value, {
-        skip: ['requestObject', 'errorMessage', '__typename'],
+        skip: [...fieldsToSkip, '__typename'],
       }),
     );
 
